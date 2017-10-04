4764cb7a5aa42835283b617062f781a932f120d9
aifi <ayffin@gmail.com>
2017-10-04 14:41:37 -0400

    checkpoint 4 assignment completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,19 +1,33 @@
 require_relative 'entry'
+
 class AddressBook
-attr_reader :entries
+  attr_reader :entries
 
-def initialize
-  @entries = []
-end
+  def initialize
+    @entries = []
+  end
 
-def add_entry(name,phone,email)
-  index = 0
-  entries.each do |entry|
-    break if name <entry.name
-    index+= 1
+  def add_entry(name,phone,email)
+    index = 0
+    entries.each do |entry|
+      break if name <entry.name
+      index+= 1
+
+    end
+    entries.insert(index, Entry.new(name,phone,email))
+  end
+
+  def remove_entry(name,phone,email)
+    index = 0
+    entries.each { |entry|
+      if name == entry.name && phone == entry.phone && email == entry.email
+        entries.delete( entry)
+          break
+        else
+          index+= 1
+      end
+    }
 
   end
-  entries.insert(index, Entry.new(name,phone,email))
-end
 
 end
