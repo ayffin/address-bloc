fb736c2b21ced19183643422bd92d4da494d4a62
aifi <ayffin@gmail.com>
2017-10-04 13:55:43 -0400

    check point 4 completed

---
+++ models/entry.rb
@@ -0,0 +1,11 @@
+class Entry
+  attr_accessor :name, :phone, :email
+  def initialize(name, phone, email)
+    @name = name
+    @phone = phone
+    @email = email
+   end
+   def to_s
+     "Name: #{name}\nPhone Number: #{phone}\nEmail: #{email}"
+   end
+ end
---
+++ spec/address_book_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../models/address_book'
+
+RSpec.describe AddressBook do
+  describe "attributes" do
+    it "responds to entries" do
+      book = AddressBook.new
+      expect(book).to respond_to(:entries)
+    end
+
+    it "initialize entries as an array" do
+      book = AddressBook.new
+      expect(book.entries).to be_an(Array)
+    end
+    it "initialize entries to an empty array" do
+      book = AddressBook.new
+      expect(book.entries.size).to eq(0)
+    end
+  end
+  describe "#add_entry" do
+    it "adds only one entry to the address_book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(1)
+    end
+    it "add the correct information to entries" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      new_entry = book.entries[0]
+
+      expect(new_entry.name).to eq('Ada Lovelace')
+      expect(new_entry.phone).to eq("010.012.1815")
+      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
+end
